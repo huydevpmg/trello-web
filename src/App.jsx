a4447f499fed73d7586e157ddc59778a75668c1f
Gia Huy Dev <huypmg.16@gmail.com>
2024-06-30 18:56:22 +0700

    mui_theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import Button from '@mui/material/Button'
 import AbcIcon from '@mui/icons-material/Abc'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
-
+import Typography  from '@mui/material/Typography'
 
 function App() {
 
   return (
     <>
       <div>giahuyphan</div>
+      <Typography variant="body2" color="text.secondary">AA</Typography>
       <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
+      <Button variant="contained" color='success'>Contained</Button>
       <Button variant="outlined">Outlined</Button>
+      <br />
       <AbcIcon/>
       <HomeIcon />
       <HomeIcon color="primary" />
